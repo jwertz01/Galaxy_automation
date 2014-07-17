a5050d5087b6db153b283a5cbcb8abb166bbe574
tombair <tom.bair@gmail.com>
2014-07-17 15:48:42 -0500

    working except rename switched to oto

--- test_script.py
+++ test_script.py
@@ -14,8 +14,8 @@ api_key = '293b01c35e9be1202446de4c0f4193b9' #galaxy.hpc api key
 #api_key = '0157d09c8a28b689702443facb93369f' #neon-galaxy.hpc api key
 galaxy_host = 'https://galaxy.hpc.uiowa.edu' #real Galaxy
 #galaxy_host ='https://neon-galaxy.hpc.uiowa.edu:44302'
-#oto_wf_id = 'c50592c2d5f1990d' #real oto workflow
-oto_wf_id = '7576cb7f105ca501'
+oto_wf_id = 'c50592c2d5f1990d' #real oto workflow
+#oto_wf_id = '7576cb7f105ca501' #simple workflow for testing
 platform_design = '808d5b008d7b5283'
 known_variants = 'd631f0584da64e36'
 oto_custom_data = '6cc40868de00e622'
@@ -49,8 +49,15 @@ R1 = toolClient.upload_file(R1,history['id'],file_type='fastqsanger')
 #datamap['14085'] = {'src':'ldda','id': R2['id']}
 #datamap['14086'] = {'src':'ldda','id': custom_quality_metrics
 print workflow['inputs']
-datamap  = {workflow['inputs'].keys()[0]: {'id': R1['outputs'][0]['id'], 'src': 'hda'}}
-params = {'SAMPLEID':sampleName}
-rep_params = {'SAMPLEID':sampleName}
+#datamap  = {workflow['inputs'].keys()[0]: {'id': R1['outputs'][0]['id'], 'src': 'hda'}} #simple datamap for testing
+datamap  = {workflow['inputs'].keys()[0]: {'id': platform_design, 'src': 'ld'},
+            workflow['inputs'].keys()[1]: {'id': known_variants, 'src': 'ld'},
+            workflow['inputs'].keys()[2]: {'id': R1['outputs'][0]['id'], 'src': 'hda'},
+            workflow['inputs'].keys()[3]: {'id': oto_custom_data, 'src': 'ld'},
+            workflow['inputs'].keys()[4]: {'id': R2['outputs'][0]['id'], 'src': 'hda'},
+            workflow['inputs'].keys()[5]: {'id': custom_quality_metrics, 'src': 'ld'},
+            }
+params = {}
+rep_params = {}
 rwf = workflowClient.run_workflow(workflow['id'],datamap,params=params,history_id=history['id'],replacement_params=rep_params)
 print rwf
