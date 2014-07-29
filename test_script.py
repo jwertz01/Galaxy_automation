9f1cb4a381d28cc0c13e7eaf2a63de24463c6209
Tom Bair <tom.bair@gmail.com>
2014-07-29 14:25:39 -0500

    starting work with new neon

--- test_script.py
+++ test_script.py
@@ -10,17 +10,17 @@ import time
 import sys
 import os.path
 
-api_key = '293b01c35e9be1202446de4c0f4193b9' #galaxy.hpc api key
-#api_key = '0157d09c8a28b689702443facb93369f' #neon-galaxy.hpc api key
-galaxy_host = 'https://galaxy.hpc.uiowa.edu' #real Galaxy
-#galaxy_host ='https://neon-galaxy.hpc.uiowa.edu:44302'
-oto_wf_id = 'c50592c2d5f1990d' #real oto workflow
-#oto_wf_id = '7576cb7f105ca501' #simple workflow for testing
-platform_design = '808d5b008d7b5283'
-known_variants = 'd631f0584da64e36'
-oto_custom_data = '6cc40868de00e622'
-report_template = '1492d625c8ec5538'
-custom_quality_metrics = '682e7654ec211682'
+#api_key = '293b01c35e9be1202446de4c0f4193b9' #galaxy.hpc api key
+api_key = 'c4ec958ebfbb56a2148f264fb89af7d5 ' #neon-galaxy.hpc api key
+#galaxy_host = 'https://galaxy.hpc.uiowa.edu' #real Galaxy
+galaxy_host ='https://neon-galaxy.hpc.uiowa.edu:44300'
+#oto_wf_id = 'c50592c2d5f1990d' #real oto workflow
+oto_wf_id = '2cd2ec6b6f5778cf' #neon oto workflow
+platform_design = '4297a1e53d8b35a4'
+known_variants = '0f299008fdceea36'
+oto_custom_data = '792cdd29e54db84f'
+report_template = 'f843e8d542196e75'
+custom_quality_metrics = '38b6e00a3fe71a99'
 R1 = sys.argv[1]
 R2 = sys.argv[2]
 sampleName = 'samName'
@@ -37,7 +37,7 @@ history = historyClient.create_history(sampleName)
 
 
 R1 = toolClient.upload_file(R1,history['id'],file_type='fastqsanger')
-#R2 = toolClient.upload_file(R2,history['id'],file_type='fastqsanger')
+R2 = toolClient.upload_file(R2,history['id'],file_type='fastqsanger')
 #Have files in place need to set up workflow
 # Based on example at http://bioblend.readthedocs.org/en/latest/api_docs/galaxy/docs.html#run-a-workflow
 #datamap = dict()
