161771fd028930edfe72ca659fa6dcb68fa1d745
Tom Bair <tom.bair@gmail.com>
2014-08-01 23:18:29 -0500

    starting work with new neon

--- test_script.py
+++ test_script.py
@@ -9,51 +9,57 @@ from bioblend.galaxy.datasets import DatasetClient
 import time
 import sys
 import os.path
+from ConfigParser import SafeConfigParser
+import re
 
+def get_api_key(file_name):
+    fh = open(file_name)
+    api = fh.readline().strip('\n')
+    return api
 
-#api_key = '293b01c35e9be1202446de4c0f4193b9' #galaxy.hpc api key
-api_key = 'c4ec958ebfbb56a2148f264fb89af7d5 ' #neon-galaxy.hpc api key
-#galaxy_host = 'https://galaxy.hpc.uiowa.edu' #real Galaxy
-galaxy_host ='https://neon-galaxy.hpc.uiowa.edu:44300'
-#oto_wf_id = 'c50592c2d5f1990d' #real oto workflow
-oto_wf_id = 'eeba014900a7fb69' #neon oto workflow
-platform_design = '4297a1e53d8b35a4'
-known_variants = '0f299008fdceea36'
-oto_custom_data = '792cdd29e54db84f'
-report_template = 'f843e8d542196e75'
-custom_quality_metrics = '38b6e00a3fe71a99'
+parser = SafeConfigParser()
+parser.read('configuration.ini')
+api_key = get_api_key(parser.get('Globals','api_file'))
+galaxy_host = parser.get('Globals','galaxy_host')
+oto_wf_id = parser.get('Globals', 'oto_wf_id')
+platform_design = parser.get('Globals','platform_design')
+known_variants = parser.get('Globals','known_variants')
+oto_custom_data = parser.get('Globals', 'oto_custom_data')
+report_template = parser.get('Globals', 'report_template')
+custom_quality_metrics = parser.get('Globals', 'custom_quality_metrics')
+file_name_re = re.compile(parser.get('Globals', 'sample_re'))
 R1 = sys.argv[1]
 R2 = sys.argv[2]
+
 try:
-    sampleName = os.path.basename(R1).split('_')[0]
+    sampleName = file_name_re.match(os.path.basename(R1)).group(1)
 except:
     sampleName = 'Unable_to_parse'
+
 
 galaxyInstance = GalaxyInstance(galaxy_host,key=api_key)
 historyClient = HistoryClient(galaxyInstance)
 toolClient = ToolClient(galaxyInstance)
 workflowClient = WorkflowClient(galaxyInstance)
 dataSetClient = DatasetClient(galaxyInstance)
-
-
 workflow = workflowClient.show_workflow(oto_wf_id)
 history = historyClient.create_history(sampleName)
 
 
 R1 = toolClient.upload_file(R1,history['id'],file_type='fastqsanger')
 R2 = toolClient.upload_file(R2,history['id'],file_type='fastqsanger')
+
 #Have files in place need to set up workflow
 # Based on example at http://bioblend.readthedocs.org/en/latest/api_docs/galaxy/docs.html#run-a-workflow
-print workflow['inputs']
 
-#datamap  = {workflow['inputs'].keys()[0]: {'id': R1['outputs'][0]['id'], 'src': 'hda'}} #simple datamap for testing
-datamap  = {workflow['inputs'].keys()[6]: {'id': platform_design, 'src': 'hda'},
-            workflow['inputs'].keys()[5]: {'id': known_variants, 'src': 'hda'},
-            workflow['inputs'].keys()[2]: {'id': R1['outputs'][0]['id'], 'src': 'hda'},
-            workflow['inputs'].keys()[4]: {'id': oto_custom_data, 'src': 'hda'},
-            workflow['inputs'].keys()[3]: {'id': R2['outputs'][0]['id'], 'src': 'hda'},
-            workflow['inputs'].keys()[1]: {'id': report_template, 'src': 'hda'},
+datamap  = {
             workflow['inputs'].keys()[0]: {'id': custom_quality_metrics, 'src': 'hda'},
+            workflow['inputs'].keys()[1]: {'id': report_template, 'src': 'hda'},
+            workflow['inputs'].keys()[2]: {'id': R1['outputs'][0]['id'], 'src': 'hda'},
+            workflow['inputs'].keys()[3]: {'id': R2['outputs'][0]['id'], 'src': 'hda'},
+            workflow['inputs'].keys()[4]: {'id': oto_custom_data, 'src': 'hda'},
+            workflow['inputs'].keys()[5]: {'id': known_variants, 'src': 'hda'},
+            workflow['inputs'].keys()[6]: {'id': platform_design, 'src': 'hda'},
             }
 params = {}
 rep_params = {'SAMPLE_ID':sampleName}
